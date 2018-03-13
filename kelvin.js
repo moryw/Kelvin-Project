6eeaf3e4dc81c73a991d51e60ac6bf31fb636921
moryw <matt@skitskat.xyz>
2018-03-13 18:12:04 -0400

    Add explanitory comments

--- kelvin.js
+++ kelvin.js
@@ -1,21 +1,22 @@
-// //this is todays forecast in kelvin
-// const kelvin = 294;
-//
-// //this is to convert kelvin to celsius
-// const celsius = kelvin - 273;
-//
-// //this converts celsius to fahrenheit
-// let fahrenheit = celsius * (9/5) + 32;
-//
-// //this rounds fahrenheit to the nearest whole number
-// fahrenheit = Math.floor(fahrenheit);
-//
-// //this logs the temperature in fahrenheit
-// console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
+//This is a program that converts todays forecast from Kelvin to Celsius and Fahrenheit
+
+//this is todays forecast in kelvin
+const kelvin = 294;
+
+//this is to convert kelvin to celsius
+const celsius = kelvin - 273;
+
+//this converts celsius to fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+
+//this rounds fahrenheit to the nearest whole number
+fahrenheit = Math.floor(fahrenheit);
+
+//this logs the temperature in fahrenheit
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
 
 
-//This is a program to convert the current temperature from Celsius to Fahrenheit
-//and Kelvin
+//This is a program to convert the current temperature from Celsius to Fahrenheit and Kelvin
 
 let celsius = -1;
 
