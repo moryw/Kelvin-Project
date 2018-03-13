0c6f9d7a94aad633273a764011f2273af1b0bd4d
moryw <matt@skitskat.xyz>
2018-03-13 18:16:50 -0400

    Add and update comments

--- kelvin.js
+++ kelvin.js
@@ -20,18 +20,24 @@ console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
 
 let celsius = -1;
 
-//this only works with browser
+//this only works with browsers not node in terminal
 //let celsius = prompt('What is the current temperature in Celsius?');
 
+//use this with a browser prompt to convert the string to an integer
 celsius = parseInt(celsius);
+
+
 console.log(`The current temperature in Celsius is ${celsius}.`);
 
+//converts celsius to kelvin
 const kelvin = celsius + 273;
 
 console.log(`The current temperature in Kelvin is ${kelvin}.`);
 
+//converts celsius to fahrenheit
 let fahrenheit = celsius * (9/5) + 32;
 
+//rounds fahrenheit to nearest whole number
 fahrenheit = Math.floor(fahrenheit);
 
 console.log(`The current temperature in Fahrenheit is ${fahrenheit}.`);
